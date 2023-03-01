f3fe45793b967fd61e62e0457172c7d92b2ebe8e
sloste34 <cocacolazero024@gmail.com>
2023-03-01 23:42:28 +0900

    Adding Regular React Components

---
+++ components/event/event-list.js
@@ -0,0 +1,1 @@
+function EventList(props) {}
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,9 @@
+import { getFeaturedEvents } from '../dummy-data';
+
 function HomePage() {
+  const featuredEvents = getFeaturedEvents();
+  console.log(featuredEvents);
+
   return (
     <div>
       <h1>Home Page</h1>
